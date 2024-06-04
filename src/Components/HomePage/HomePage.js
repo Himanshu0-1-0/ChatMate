51ac4361e99bd5559b53778b466f4b8a53ef4c0d
Himanshu <himanshumehta8104@gmail.com>
2024-06-04 16:33:10 +0530

    Home and Login Page Integrated

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,13 +1,15 @@
 import "./HomePage.css"
 import Signin from "../Signin/Signin"
+import Link from "next/link"
 export default function HomePage() {
   return (
     <>
       <div className="cont">
-        <div className="bg">
-        </div>
+        <img src="/Logo.png" alt="Logo" />
+        <button className="btn" ><Link href="/Signin" className="linkk">Login/Signup</Link></button>
+        <div className="bg"></div>
       </div>
-      <Signin/>
+      {/* <Signin id="signin"/> */}
     </>
   )
 }
---
+++ src/app/Signin/page.js
@@ -0,0 +1,8 @@
+import Signin from "../../Components/Signin/Signin"
+export default function page() {
+  return (
+    <>
+        <Signin/> 
+    </>
+  )
+}
